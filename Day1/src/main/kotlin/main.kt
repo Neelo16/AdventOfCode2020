3ac1efa18f087165b2ac43880c36dfef118bbc3d
Tomás Cunha <tomas.cunha@tecnico.ulisboa.pt>
2020-12-01 16:33:25 +0000

    Solve day 1

---
+++ Day1/src/main/kotlin/main.kt
@@ -0,0 +1,30 @@
+fun getResourceAsText(path: String): String {
+    return object {}.javaClass.getResource(path).readText()
+}
+
+fun findMult(values: IntArray, target: Int, numVals: Int, prevVals: IntArray): Int {
+    if (numVals == 1) {
+        for (head in values)
+            if (prevVals.sum() + head == target)
+                return prevVals.reduce { acc, it -> acc * it } * head
+        return 0
+    } else {
+        for ((i, head) in values.withIndex()) {
+            val result = findMult(values.sliceArray(IntRange(i, values.size-1)), target, numVals-1, intArrayOf(*prevVals, head))
+            if (result != 0) {
+                return result
+            }
+        }
+    }
+    return 0
+}
+
+fun main(args: Array<String>) {
+    val target = 2020
+    val input = getResourceAsText("/input.txt")
+    val values = input.trim().split("\n").map { it.toInt() }.sorted().toIntArray()
+    print("Part 1: ")
+    println(findMult(values, target, 2, IntArray(0)))
+    print("Part 2: ")
+    println(findMult(values, target, 3, IntArray(0)))
+}
